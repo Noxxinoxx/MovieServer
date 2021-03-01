0c24ce6f0fb2fea0d539523e5c2efde523a90c4b
Noa Andersson <34545334+nooaandersson@users.noreply.github.com>
2021-03-01 03:11:07 +0100

    dsf
    sdf

--- index.js
+++ index.js
@@ -4,34 +4,71 @@ var app = express();
 var server = http.createServer(app)
 var io = require('socket.io')(server);
 var fs = require("fs")
-app.use(express.static('public'))
+
+
 
 app.get("/", (req, res) => {
     res.sendFile("index.html", {root: __dirname})
-
+    
 })
 
-var file = 0;
+
 var users = []
-var videos = []
-fs.readdirSync("./public/Movies/", (file) => {
-    videos.push(file)
+let randomID;
+let movieClicked;
+
+io.of("/Wonder").on("connection", (socket) => {
+    socket.emit("renderMovieFromHandleBars", {Title: movieClicked, ID: randomID})
+    console.log("jjj")
+    app.use('/static/' + randomID, express.static('public'))
 })
-console.log(videos)
-io.on("connection", (socket) => {
+
+io.of("/Movies").on("connection", (socket) => {
+    var MovieFolder = fs.readdirSync("./public/Movie/")
+    socket.emit("videos", MovieFolder)
+    socket.on("disconnect", (user) => {
+       
+        console.log("Video viewer disconneced")
+    })
+
+    
+    socket.on("videoClicked", (data) => {
+        socket.emit("movie", {Title: data.MovieClicked, ID: randomID})
+        movieClicked = data.MovieClicked;
+        app.get("/" + data.MovieClicked, (req, res) => {
+            console.log(req.query.id)
+            console.log(randomID)
+            if(req.query.id == randomID){
+                res.sendFile("test.html", {root: __dirname})  
+                  
+                
+            }else {
+                console.log("hehe")
+                res.status(404)
+            }
+
+        })
+    })
+    
+    
+})
+io.of("/").on("connection", (socket) => {
+    randomID = Math.random(10000)
     socket.on("UserLogin", (data) =>{
         if(data.username == "Nox" && data.password == "NoxPass") {
-            var randomID = Math.random(10000)
             socket.emit("verified", {verifed : true, ID: randomID})
             users.push(data.username)
-            app.get("/Movies", (req, res) => {
+            app.get("/Movies/", (req, res) => {
+                
                 if(req.query.id == randomID){
                     
                     res.sendFile("Movie.html", {root: __dirname})
                     
+                }else {
+                    res.status(404)
                 }
             })
-            socket.emit("videos", videos)
+            
         }else {
             socket.emit("verified", {verifed: false})
         }
@@ -40,6 +77,8 @@ io.on("connection", (socket) => {
     socket.on("disconnect",(user) => {
         console.log("user disconneected")
         users = []
+        
+        
     })
 })
 
